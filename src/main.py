2b22ed1af32ca2b898adb0cff86e60038b48ae6b
Evgeniy <ujenreznik@gmail.com>
2015-07-25 16:11:30 +0300

    include done

--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@ Created on Jul 25, 2015
 @author: Evgeniy
 '''
 
+
+import time, pyaudio, wave, os, urllib, urllib2, pycurl, httplib, sys, win32api, win32con, string
+from ctypes import *
+
 if __name__ == '__main__':
     print("hi i am alive")
     print("first change")

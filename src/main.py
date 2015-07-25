7cb91d7239f2ebcae52e592a805e20ea067c78e8
Evgeniy <ujenreznik@gmail.com>
2015-07-25 17:21:51 +0300

    added send to server function

--- src/main.py
+++ src/main.py
@@ -15,29 +15,91 @@ from ctypes import *
 
 def Talk(text):
  
- def downloadFile(url, fileName):
-    fp = open(fileName, "wb")
-    curl = pycurl.Curl()
-    curl.setopt(pycurl.URL, url)
-    curl.setopt(pycurl.WRITEDATA, fp)
-    curl.perform()
-    curl.close()
-    fp.close()
+    def downloadFile(url, fileName):
+        fp = open(fileName, "wb")
+        curl = pycurl.Curl()
+        curl.setopt(pycurl.URL, url)
+        curl.setopt(pycurl.WRITEDATA, fp)
+        curl.perform()
+        curl.close()
+        fp.close()
 
- def getGoogleSpeechURL(phrase):
-    googleTranslateURL = "http://translate.google.com/translate_tts?tl=en&"
-    parameters = {'q': phrase}
-    data = urllib.urlencode(parameters)
-    googleTranslateURL = "%s%s" % (googleTranslateURL,data)
-    return googleTranslateURL
+    def getGoogleSpeechURL(phrase):
+        googleTranslateURL = "http://translate.google.com/translate_tts?tl=en&"
+        parameters = {'q': phrase}
+        data = urllib.urlencode(parameters)
+        googleTranslateURL = "%s%s" % (googleTranslateURL,data)
+        return googleTranslateURL
 
- def speakSpeechFromText(phrase):
-    googleSpeechURL = getGoogleSpeechURL(phrase)
-    downloadFile(googleSpeechURL,"ans.mp3")
- 
- speakSpeechFromText(text)
- pass
+    def speakSpeechFromText(phrase):
+        googleSpeechURL = getGoogleSpeechURL(phrase)
+        downloadFile(googleSpeechURL,"ans.mp3")
 
+    speakSpeechFromText(text)
+    pass
+
+def Record():
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 16000
+    RECORD_SECONDS = 5
+    WAVE_OUTPUT_FILENAME = "output.wav"
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+    print("Recording...")
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("Done recording.")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+def Convert():
+    print "Converting"
+    os.system('C:\Users\Егор\Desktop\Расширение\TotalAudioConverter\AudioConverter.exe C:\Users\Егор\Desktop\Расширение\output.wav C:\Users\Егор\Desktop\Расширение\output.flac')
+    print "Done"
+
+def Send():
+    global ANSWER
+    url = 'https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-EN'
+    flac=open('output.flac',"rb").read()
+    header = {'Content-Type' : 'audio/x-flac; rate=16000'}
+    req = urllib2.Request(url, flac, header)
+    data = urllib2.urlopen(req)
+    a = data.read()
+    ANSWER = eval(a)
+    if ANSWER['status'] == 5:
+        print 'Sorry, I do not understand you.'
+        Talk('Sorry, I do not understand you.')
+        ANSWER = 0
+
+    else:
+        ANSWER = ANSWER['hypotheses'][0]['utterance']
+        print ANSWER
+    os.remove('C:\Users\Егор\Desktop\Расширение\output.wav')
+    os.remove('C:\Users\Егор\Desktop\Расширение\output.flac')
+    return ANSWER
 
 if __name__ == '__main__':
     print("hi i am alive")
